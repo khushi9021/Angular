7f2ab4fbe5822cf862e3a3c7e3acb46698b924a8
khushi <94215052+khushi9021@users.noreply.github.com>
2022-02-19 16:48:29 +0530

    Add files via upload

---
+++ api.service.ts
@@ -0,0 +1,27 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class APIService {
+
+  baseurl = "http://localhost:8081/";
+
+
+  constructor(private http: HttpClient) {
+
+  }
+  post = (api: string, data: any) => {
+    const headers = {'content-type': 'application/json'}
+    const body = JSON.stringify(data);
+    return this.http.post(this.baseurl + api, body, {'headers':headers});
+
+  }
+
+  get = (api: string, data: any) => {
+    return this.http.get(this.baseurl + api, data);
+
+  }
+
+}
